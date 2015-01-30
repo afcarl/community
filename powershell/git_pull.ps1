6de016dbb1a64b4ec61be1e430fba4e6fd35ebd0
Chris Joakim <christopher.joakim@gmail.com>
2015-01-30 06:45:20 -0500

    Added an example git pull PowerShell script, with Windows shortcut instructions.

---
+++ powershell/git_pull.ps1
@@ -0,0 +1,16 @@
+﻿
+# "Git Pull" - Windows Desktop Shortcut:
+#   Target: C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -File "C:\si\svp\git_pull.ps1"
+#   powershell.exe, start in: C:\Windows\System32\WindowsPowerShell\v1.0
+#   Chris Joakim, 2015/01/30
+
+echo "Git Pull ..."
+
+cd  C:\si\svp
+git reset --hard
+git pull 
+
+Write-Host "Press any key to exit ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+echo "done"
